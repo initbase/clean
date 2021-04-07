660043630aa7afea350120b1898c2682cba97127
burhanahmeed <burhanahmeed@gmail.com>
2021-04-07 10:16:06 +0700

    add checks components

---
+++ docs/components/checksradios/Indeterminate.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card p-3">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate">
+      <label class="form-check-label" for="flexCheckIndeterminate">
+        Indeterminate checkbox
+      </label>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  mounted () {
+    document.getElementById("flexCheckIndeterminate").indeterminate = true;
+  }
+}
+</script>
+<style scoped>
+@import '../../../src/components/theme.css';
+</style>
---
+++ docs/components/select/Sizing.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card p-4">
+    <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+      <option selected>Open this select menu</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </select>
+
+    <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+      <option selected>Open this select menu</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </select>
+  </div>
+</template>
+<style scoped>
+@import '../../../src/components/_select.css';
+@import '../../../src/components/_forms.css';
+@import '../../../src/components/_card.css';
+</style>
